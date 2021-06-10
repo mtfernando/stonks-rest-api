155db49e329660debafe260c4494c9d1757cebf2
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-06-10 14:19:19 +1000

    basic error handling added

--- stonks_rest_api.py
+++ stonks_rest_api.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 
 @app.route("/<ticker>")
 def evaluate(ticker):
-    ticker = ticker.upper()
-    return get_data(Console(), ticker, 10)
 
+    ticker = ticker.upper()
+    try:
+        company_data =  get_data(Console(), ticker, 10)
+    
+    except:
+        company_data = null
+
+    return company_data
+    
 @app.route("/")
 def home():
     return "<h2>Welcome to the Stonks REST API</h2>"
